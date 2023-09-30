63a046a17b9a4a22195711cd6be813996fa199a2
Luanderson Alves <luaoderson@gmail.com>
2023-09-29 21:17:14 -0300

    perf: exception treatment

--- bot_login/bot_login.py
+++ bot_login/bot_login.py
@@ -3,40 +3,97 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 import json
+import os
+import time
+
+
+def read_data_json():
+    script_dir = os.path.dirname(__file__)
+    json_path = os.path.join(script_dir, "user.json")
+    with open(json_path) as file:
+        return json.load(file)
+
+
+data = read_data_json()
+
+WAIT_TIME = data["wait_time"]
+
+MAX_RETRIES = data["max_retries"]
+
+USERNAME = data["username"]
+
+PASSWORD = data["password"]
+
+
+def find_element_with_retry(
+    driver, by, value, max_retries=MAX_RETRIES, wait_time=WAIT_TIME
+):
+    retries = 0
+    while retries < max_retries:
+        try:
+            element = driver.find_element(by, value)
+            wait = WebDriverWait(driver, wait_time)
+            wait.until(lambda d: element.is_displayed())
+            return element
+
+        except NoSuchElementException:
+            retries += 1
+            print(
+                f'''Elemento "{value}" não encontrado,
+                tentando novamente ({retries}/{max_retries})...'''
+            )
+            time.sleep(wait_time)
+    raise NoSuchElementException(
+        f'''Elemento "{value}" não encontrado após
+        {max_retries} tentativas. O script foi encerrado.'''
+    )
 
 
 def login():
-    with open("bot_login/user.json") as file:
-        json_file = json.load(file)
+    try:
+        driver = webdriver.Edge()
 
-        USERNAME = json_file["username"]
-        PASSWORD = json_file["password"]
+        driver.maximize_window()
 
-    TIMER = 5
+        driver.get("https://www.instagram.com/")
 
-    driver = webdriver.Edge()
+        input_username = find_element_with_retry(
+            driver, By.XPATH, '//input[@name="username"]'
+        )
 
-    driver.maximize_window()
+        input_username.send_keys(USERNAME)
 
-    driver.get("https://www.instagram.com/")
+        input_password = find_element_with_retry(
+            driver, By.XPATH, "//input[@name='password']"
+        )
 
-    driver.implicitly_wait(TIMER)
+        input_password.send_keys(PASSWORD)
 
-    driver.find_element(By.XPATH, '//input[@name="username"]').send_keys(USERNAME)
+        btn_login = find_element_with_retry(
+            driver, By.XPATH, '//button[@type="submit"]'
+        )
 
-    driver.find_element(By.XPATH, "//input[@name='password']").send_keys(PASSWORD)
+        btn_login.click()
 
-    driver.find_element(By.XPATH, '//button[@type="submit"]').click()
+        first_popup = find_element_with_retry(
+            driver, By.XPATH, '//div[contains( text(),"Agora não")]'
+        )
 
-    driver.implicitly_wait(TIMER * 3)
+        first_popup.click()
 
-    driver.find_element(By.XPATH, '//div[contains( text(),"Agora não")]').click()
+        second_popup = find_element_with_retry(
+            driver, By.XPATH, '//button[contains( text(),"Agora não")]'
+        )
 
-    driver.find_element(By.XPATH, '//button[contains( text(),"Agora não")]').click()
+        second_popup.click()
 
-    input('Pressione "Enter" para fechar o navegador...')
+        input('Pressione "Enter" para fechar o navegador...')
 
-    driver.quit()
+    except NoSuchElementException as e:
+        print(e)
+
+    finally:
+        driver.quit()
 
 
 if __name__ == "__main__":
